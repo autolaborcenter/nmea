e0803071d5943bdbc40b92ba398b39b88252eeb0
YdrMaster <ydrml@hotmail.com>
2021-11-22 15:20:27 +0800

    fix: 应对 GPGGA 的空包特例

--- src/gpgga.rs
+++ src/gpgga.rs
@@ -1,7 +1,7 @@
 ﻿use super::BodyParseError;
 use std::str::FromStr;
 
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq, Debug)]
 pub struct Body {
     utc_time: u32,
     latitude: (u64, u8),
@@ -19,19 +19,19 @@ pub struct Body {
     diff_station: Option<u16>,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq, Debug)]
 pub enum NS {
     N,
     S,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq, Debug)]
 pub enum EW {
     E,
     W,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq, Debug)]
 pub enum Status {
     初始化 = 0x0,
     单点定位 = 0x1,
@@ -44,7 +44,7 @@ pub enum Status {
     WAAS差分 = 0x9,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq, Debug)]
 pub enum LenUnit {
     M,
 }
@@ -53,23 +53,42 @@ impl FromStr for Body {
     type Err = BodyParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut s = s.split(',');
-        Ok(Self {
-            utc_time: parse_field!(s => "GPGGA:UTCTime"; 2),
-            latitude: parse_field!(s => "GPGGA:Latitude"; ?),
-            ns: parse_field!(s => "GPGGA:N"),
-            longitude: parse_field!(s => "GPGGA:Longitude"; ?),
-            ew: parse_field!(s => "GPGGA:E"),
-            status: parse_field!(s => "GPGGA:FS"),
-            nosv: parse_field!(s => "GPGGA:NoSV"),
-            hdop: parse_field!(s => "GPGGA:HDOP"; ?),
-            altitude: parse_field!(s => "GPGGA:Altitude"; ?),
-            alt_unit: parse_field!(s => "GPGGA:AltUnit"),
-            alt_ref: parse_field!(s => "GPGGA:Altref"; ?),
-            alt_ref_unit: parse_field!(s => "GPGGA:AltrefUnit"),
-            diff_age: parse_field!(s =>? "GPGGA:DiffAge"),
-            diff_station: parse_field!(s =>? "GPGGA:DiffStation"),
-        })
+        if s == ",,,,,0,,,,,,,," {
+            Ok(Self {
+                utc_time: 0,
+                latitude: (0, 0),
+                ns: NS::N,
+                longitude: (0, 0),
+                ew: EW::E,
+                status: Status::初始化,
+                nosv: 0,
+                hdop: (0, 0),
+                altitude: (0, 0),
+                alt_unit: LenUnit::M,
+                alt_ref: (0, 0),
+                alt_ref_unit: LenUnit::M,
+                diff_age: None,
+                diff_station: None,
+            })
+        } else {
+            let mut s = s.split(',');
+            Ok(Self {
+                utc_time: parse_field!(s => "GPGGA:UTCTime"; 2),
+                latitude: parse_field!(s => "GPGGA:Latitude"; ?),
+                ns: parse_field!(s => "GPGGA:N"),
+                longitude: parse_field!(s => "GPGGA:Longitude"; ?),
+                ew: parse_field!(s => "GPGGA:E"),
+                status: parse_field!(s => "GPGGA:FS"),
+                nosv: parse_field!(s => "GPGGA:NoSV"),
+                hdop: parse_field!(s => "GPGGA:HDOP"; ?),
+                altitude: parse_field!(s => "GPGGA:Altitude"; ?),
+                alt_unit: parse_field!(s => "GPGGA:AltUnit"),
+                alt_ref: parse_field!(s => "GPGGA:Altref"; ?),
+                alt_ref_unit: parse_field!(s => "GPGGA:AltrefUnit"),
+                diff_age: parse_field!(s =>? "GPGGA:DiffAge"),
+                diff_station: parse_field!(s =>? "GPGGA:DiffStation"),
+            })
+        }
     }
 }
 
