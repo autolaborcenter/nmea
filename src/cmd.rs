67ecbb28945c8220c2e19f826a8408e4c498d077
YdrMaster <ydrml@hotmail.com>
2021-11-24 09:50:19 +0800

    feat: 去掉 cmd,through

--- src/cmd.rs
+++ src/cmd.rs
@@ -5,8 +5,7 @@ use std::str::FromStr;
 pub enum Body {
     Get(String),
     Set(String),
-    Through(String),
-    Unknown(String, String),
+    Undefined(String, String),
 }
 
 impl FromStr for Body {
@@ -17,8 +16,7 @@ impl FromStr for Body {
             Some((head, tail)) => match head {
                 "get" => Ok(Body::Get(tail.into())),
                 "set" => Ok(Body::Set(tail.into())),
-                "through" => Ok(Body::Through(tail.into())),
-                unknown => Ok(Body::Unknown(unknown.into(), tail.into())),
+                unknown => Ok(Body::Undefined(unknown.into(), tail.into())),
             },
             None => Err(BodyParseError::MissingField("CMD:Type")),
         }
@@ -30,8 +28,7 @@ impl ToString for Body {
         match self {
             Self::Get(body) => fmt_line("get", body),
             Self::Set(body) => fmt_line("set", body),
-            Self::Through(body) => fmt_line("through", body),
-            Self::Unknown(head, body) => fmt_line(head, body),
+            Self::Undefined(head, body) => fmt_line(head, body),
         }
     }
 }
